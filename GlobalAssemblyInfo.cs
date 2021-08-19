10cff4b7915bedc5208d25ec9b535e1cecc1b94c
Zhanho Shing <zhanhoshing@icloud.com>
2021-08-20 04:24:55 +0800

    feat: added new project

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using PopcatClient;
+
+[assembly: AssemblyInformationalVersion(AssemblyData.InformationalVersion)]
+
+namespace PopcatClient
+{
+    public static class AssemblyData
+    {
+        public const string InformationalVersion = "v0.1-beta.3";
+    }
+}
