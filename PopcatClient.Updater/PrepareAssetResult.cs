0a75522b1628e465737c31de48c9240c1cc3f62c
Zhanho Shing <zhanhoshing@icloud.com>
2021-08-22 12:43:46 +0800

    feat: created prepare asset result class

---
+++ PopcatClient.Updater/PrepareAssetResult.cs
@@ -0,0 +1,15 @@
+﻿namespace PopcatClient.Updater
+{
+    /// <summary>
+    /// Contains information about the result of the task of preparing
+    /// </summary>
+    public class PrepareAssetResult : BasicTaskResult
+    {
+        /// <summary>
+        /// Represents the status of the result
+        /// </summary>
+        public BasicResultStatus Status { get; internal set; }
+        public string ExtractedDir { get; internal set; }
+        public string InstallerExecutable { get; internal set; }
+    }
+}
